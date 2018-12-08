96690dd95c46e264dc0701ca35332bc935ecdfde
qq1257787262 <1257787262@qq.com>
2018-12-08 16:12:46 +0800

    第二天代码

--- day2.0/feedback/app.js
+++ day2.0/feedback/app.js
@@ -4,6 +4,8 @@ var http = require('http')
 var fs =require('fs')
 //导入模板引擎
 var template =require('art-template')
+//url模块
+var url=require("url")
 
 //创建假数据数组
 var comments=[
@@ -28,15 +30,18 @@ var comments=[
         datetime:'2018-12-07'
     }
 ]
+//对我们来讲，其实至于要判定，如果你的请求路径是/pinglun就可以
 
 //创建服务
 http
 .createServer(function (req,res){
-    //获取url
-    var url =req.url
-
+    // //获取url
+    // var url =req.url
+    //使用url.parse方法将路径解析为一个方便操作的对象，第二个参数为true，表示直接讲查询字符串被转化为一个对象（通过query属性来访问）
+    var parseObj=url.parse(req.url,true)
+    var pathname=parseObj.pathname
     //根据url做出响应
-    if(url==="/"){
+    if(pathname==="/"){
         //读取对应html文件
         fs.readFile('./view/index.html',function(err,data){
             //判断如果出错，返回404
@@ -52,15 +57,15 @@ http
             res.end(htmlStr)
         })   
     }
-    else if(url.indexOf('/public/')===0){
-        fs.readFile('.'+url,function(err,data){
+    else if(pathname.indexOf('/public/')===0){
+        fs.readFile('.'+pathname,function(err,data){
             if(err){
                 return res.end('404 Not Found')
             }
             res.end(data)
         })
     }
-    else if(url==='/post'){
+    else if(pathname==='/post'){
 
         fs.readFile('./view/post.html',function(err,data){
             if(err){
@@ -68,6 +73,22 @@ http
             }
             res.end(data)
         })
+    }
+    else if(pathname==="/pinglun"){
+        //一次请求对应一次相应，相应结束这次请求也就结束
+        // res.end(JSON.stringify(parseObj.query))
+        //获取表单提交数据parseObj。query
+        //生成日期到数据对象中，然后储存到数组中
+        //让用户重新跳转到首页
+        var comment=parseObj.query
+        comment.datetime="asdfafad"
+        comments.unshift(comment)
+        //如何通过服务器让客户端重定向
+        //1.状态码设置为302临时重定向
+        // 2.在响应头中通过lacation告诉客户端往哪重定向setheader
+        res.statusCode=302
+        res.setHeader("location","/")
+        res.end()
     }
     else{
         fs.readFile('./view/404.html',function(err,data){
