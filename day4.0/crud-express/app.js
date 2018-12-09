1a2bff89655e474e7d256fadb28bea7535612163
qq1257787262 <1257787262@qq.com>
2018-12-10 00:08:59 +0800

    第四天上午

---
+++ day4.0/crud-express/app.js
@@ -0,0 +1,27 @@
+var express =require('express')
+var fs = require('fs')
+
+var app = express()
+
+app.engine('html',require('express-art-template'))
+
+app.use('/node_modules/',express.static('./node_modules'))
+app.use('/public/',express.static('./public'))
+
+app.get('/', function(req,res){
+    //readFile 的第二个可选参数，传入utm8 即可不用tostring()
+    fs.readFile('./db.json',"utf8",function(err,data){
+        if(err){
+            return res.status(500).send('Server error')
+        }
+
+        res.render('index.html',{
+            students: JSON.parse(data).students
+        })
+    })
+    
+})
+
+app.listen(3000,function(){
+    console.log('歪歪真帅')
+})
